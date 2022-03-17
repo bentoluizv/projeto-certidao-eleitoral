27068eaace1f03ea2cc8ac26244faa20e979eeb0
bentoluizv <bentoluizv@gmail.com>
2022-03-17 15:39:17 -0300

    definido os metodos acessores para o situacao referente a situacao eleitoral do eleitor.

--- eleitor.py
+++ eleitor.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from re import M
 
 
 class Eleitor():
@@ -22,14 +23,28 @@ class Eleitor():
             self.secao, self.municipio, self.uf, self.data_insc, self.votou] = dados
         self.set_idade()
         self.set_info()
+        self.set_situacao()
 
         pass
 
     def set_idade(self):
-        nasc = self.data_nasc[-4::]
-        ano_atual = datetime.now().strftime('%Y')
-        self.idade = int(ano_atual) - int(nasc)
-        pass
+        ano_nasc = int(self.data_nasc[-4::])
+        mes_nasc = int(self.data_nasc[3:5])
+        dia_nasc = int(self.data_nasc[0:2])
+        ano_atual = int(datetime.now().strftime('%Y'))
+        mes_atual = int(datetime.now().strftime('%m'))
+        dia_atual = int(datetime.now().strftime('%d'))
+
+        idade = ano_atual - ano_nasc
+        if mes_nasc == 2 and dia_nasc == 29:
+            dia_nasc = dia_nasc - 1
+        if mes_atual < mes_nasc:
+            idade = idade - 1
+        elif mes_atual == mes_nasc:
+            if dia_atual < dia_nasc:
+                idade = idade - 1
+
+        self.idade = idade
 
     def get_idade(self):
         return self.idade
@@ -43,7 +58,13 @@ class Eleitor():
         return self.info
 
     def set_situacao(self):
+        idade = self.get_idade()
+        votou = self.get_info()[-1]
+        obrigatorio = True if (idade >= 18 and idade < 70) else False
+        situacao = 'Irregular' if (
+            votou == '0' and obrigatorio == True) else 'Regular'
+        self.situacao = situacao
         pass
 
     def get_situacao(self):
-        pass
+        return self.situacao
